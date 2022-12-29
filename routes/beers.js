1896fac89bb3353fc3d697de502222fbb9b46a0b
Jill Arnold <jillarnold2913@gmail.com>
2022-12-29 13:29:01 -0500

    Example of using param middleware

--- routes/beers.js
+++ routes/beers.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+//Normally you would be using a database not an array of 
+//objects as below
 const beers = [
   { id: '1', name: 'IPA' },
   { id: '2', name: 'Stout' },
@@ -8,12 +10,17 @@ const beers = [
   { id: '4', name: 'Pilsner' }
 ];
 
+//PARAM MIDDLEWARE (param must match up with route params
+//below) this router.param allows us to reuse this function
+//since it will be needed over and over again
 router.param('beer', function(req, res, next, id) {
   req.beer = beers.find(beer => beer.id === id);
 
   next();
 });
 
+//Route below does not have to specify "beers" because 
+//it is implied since we are using the router in beers.js
 router.get('/', (req, res) => {
   res.send('hello from beers!');
 });
